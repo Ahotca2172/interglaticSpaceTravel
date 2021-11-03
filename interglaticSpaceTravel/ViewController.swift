978f45bf372df749b87179fe19e13b684165fd3c
Alex Hotca <ahotca2172@stu.d214.org>
2021-11-03 14:04:40 -0500

    Initial Commit

---
+++ interglaticSpaceTravel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  interglaticSpaceTravel
+//
+//  Created by Alex Hotca on 11/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
